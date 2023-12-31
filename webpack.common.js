39d08812a49a074ea6bf2eecd200bdf64a33a02a
GigaVardia <gigavardia@gmail.com>
2023-12-31 01:19:49 -0800

    renamed output folder

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
 
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public"),
         clean: true,
         assetModuleFilename: "assets/img/[name][ext][query]", // without this assets used in js/css will go to main dir
     },
